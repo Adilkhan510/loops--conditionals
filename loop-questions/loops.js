47b84911d5bd43349352960106c459061105a1ed
adilkhan <adilkhan@ucsb.edu>
2020-06-06 20:00:51 -0700

    commiting to save

---
+++ loop-questions/loops.js
@@ -0,0 +1,40 @@
+let num = 99;
+while (num >= 0) {
+    console.log(num);
+    num -= 1;
+}
+
+let num2 = 0;
+while (num2 <= 99) {
+    console.log(num2);
+    num2++;
+}
+
+
+for (let i=298; i>=1; i-=3) {
+    console.log(i);
+}
+
+for (let i=0; i%2 === 0 && i<=98; i+=2) {
+    console.log(i);
+}
+
+for (let i=98; i>=0; i-=1) {
+    if(i%2 !==0) {
+        console.log(i)
+    }
+
+}
+
+for (i=49; i<=72; i++) {
+    console.log(i);
+}
+
+for (i=81; i>=26; i-=1) {
+    console.log(i);
+}
+for (i=1; i<=91; i++) {
+    if(i%3==0) {
+        console.log(i)
+    };
+} 
